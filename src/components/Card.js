f7c033128258c16f99ad78f2fec0af50188dbdab
Tanveer Pathan <125748515+TanveerSP@users.noreply.github.com>
2023-12-13 08:57:04 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+
+function Card({id, image, info, price, name, removeTour }) {
+  
+  const [readmore, setRedmore] = useState(false);
+  
+  const description = readmore ? info : `${info.substring(0, 200)}....`;
+  
+  function readmoreHnadler() {
+    setRedmore(!readmore);
+  }
+
+  return (
+
+    <div className="card">
+
+      <img src={image} alt="" className="image" />
+
+      <div className="tour-info">
+
+            <div className="tour-details">
+                <h4 className="tour-price">&#8377; {price}</h4>
+                <h4 className="tour-name">{name}</h4>
+            </div> 
+
+            <div className="description">
+                {description}
+                <span className="read-more" onClick={readmoreHnadler}>
+                    {readmore ?  "Read More" : "Show Less"}
+                </span>
+            </div>
+
+      </div>
+
+      <button className="btn-red" onClick={() => removeTour(id)}>
+        Not Interested
+      </button>
+
+    </div>
+  );
+  
+}
+
+export default Card;
---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+  {
+    id: 1,
+    name: "Agra",
+    info: "Agra, city, western Uttar Pradesh state, northern India. It lies in the Indo-Gangetic Plain on the Yamuna (Jumna) River about 125 miles (200 km) southeast of Delhi. There was an early reference to an Agravana in the ancient Sanskrit epic Mahabharata, and Ptolemy is said to have called the site “Agra.” The city was founded by Sultan Sikandar of the Lodī dynasty in the early 16th century to be the capital of the Delhi sultanate. Agra also served as the Mughal capital during some periods of that empire",
+    image: "https://cdn.thecodehelp.in/Agra.jpeg",
+    price: "35,758",
+  },
+  {
+    id: 2,
+    name: "Jaipur",
+    info: " Jaipur is the capital city of the Indian state of Rajasthan, known Jaipur’s most-distinctive landmark, the Hawa Mahal is an extraordinary pink-painted, delicately honeycombed hive that rises a dizzying five storeys. It was constructed in 1799 by Maharaja Sawai Pratap Singh to enable ladies of the royal household to watch the life and processions of the city. The top offers stunning views over Jantar Mantar and the City Palace in one direction and over Sireh Deori Bazaar in the other.",
+    image: "https://cdn.thecodehelp.in/Jaipur.jpeg",
+    price: "82,560",
+  },
+  {
+    id: 3,
+    name: "Goa",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, officiis. Voluptatibus, maxime molestiae! Quia officiis iure, suscipit maxime nostrum eligendi!",
+    image: "https://cdn.thecodehelp.in/Goa.jpeg",
+    price: "29,695",
+  },
+  {
+    id: 4,
+    name: "Varanasi",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, officiis. Voluptatibus, maxime molestiae! Quia officiis iure, suscipit maxime nostrum eligendi!",
+    image: "https://cdn.thecodehelp.in/Varanasi.jpeg",
+    price: "29,695",
+  },
+  {
+    id: 5,
+    name: "Darjeeling",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, officiis. Voluptatibus, maxime molestiae! Quia officiis iure, suscipit maxime nostrum eligendi!",
+    image: "https://cdn.thecodehelp.in/Darjeeling.jpeg",
+    price: "29,695",
+  },
+  {
+    id: 6,
+    name: "Jaisalmer",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, officiis. Voluptatibus, maxime molestiae! Quia officiis iure, suscipit maxime nostrum eligendi!",
+    image: "https://cdn.thecodehelp.in/Jaisalmer.jpeg",
+    price: "29,695",
+  },
+  {
+    id: 6,
+    name: "Kochi",
+    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, officiis. Voluptatibus, maxime molestiae! Quia officiis iure, suscipit maxime nostrum eligendi!",
+    image: "https://cdn.thecodehelp.in/Kochi.jpeg",
+    price: "29,695",
+  },
+];
+
+export default data;
